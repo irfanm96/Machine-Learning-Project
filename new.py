c98fe7381259a44b25680df21f4978317f0dd34c
irfanm96 <e15138@ce.pdn.ac.lk>
2020-04-02 14:40:01 +0530

    emergency updates

--- new.py
+++ new.py
@@ -96,7 +96,7 @@ X_train, X_test, y_train, y_test = train_test_split(r.values, target, test_size=
 
 # Create a Gaussian Classifier
 model = GaussianNB()
-model.fit(X_train, y_train)
+model.fit(r.values, target)
 
 # # # Predict the response for test dataset
 y_pred = model.predict(X_test)

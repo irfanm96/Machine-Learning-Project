94290c0302ae9df430cc1dcb5a44804992cee308
Hisni <hisnionline@gmail.com>
2020-04-28 13:16:13 +0530

    repo cleanup

--- project.py
+++ project.py
@@ -5,19 +5,17 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 # Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
-
 
 def impute_nominal_attributes(data_frame, columns, strategy="most_frequent"):
     impurer = SimpleImputer(strategy=strategy)
     return impurer.fit_transform(data_frame[columns])
 
-
 def impute_numerical_attributes(data_frame, columns, strategy="mean"):
     impute = SimpleImputer(missing_values=np.nan, strategy=strategy)
     return impute.fit_transform(data_frame[columns])
-
 
 def merge_arrays_to_frames(array1, array1_columns, array2, array2_columns):
     a = pd.DataFrame(array1)
@@ -28,9 +26,7 @@ def merge_arrays_to_frames(array1, array1_columns, array2, array2_columns):
 
     return pd.concat([a, b], axis=1)
 
-
 encoder = OneHotEncoder(dtype=int, sparse=True)
-
 
 def encode_nominal_attributes(data_frame, columns, type='train'):
     if type == 'train':
@@ -42,11 +38,9 @@ def encode_nominal_attributes(data_frame, columns, type='train'):
     nominal.columns = encoder.get_feature_names().tolist()
     return nominal
 
-
 def discretize_numerical_attributes(data_frame, columns):
     data_frame[columns].dtype = float
     return data_frame[columns]
-
 
 def pre_processing(original, type='train'):
     original.replace({'?': np.NaN}, inplace=True)
@@ -69,7 +63,6 @@ def pre_processing(original, type='train'):
     nu = discretize_numerical_attributes(k, numerical_columns)
     return pd.concat([no, nu], axis=1)
 
-
 '''
 We cannot use ordinal encoder since we dont know which are ordinal in those nominal attributes
 '''
@@ -88,41 +81,76 @@ r = pre_processing(data)
 target = le.fit_transform(label)
 
 # Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(r.values, target, test_size=0.3,
-                                                    random_state=109)  # 70% training and 30% test
-# set this as training data
-# X_train = data
+# 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(r.values, target, test_size=0.3,random_state=109)  
 
+# Scaling X_train and X_test
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX_train = scaler.fit_transform(X_train)
+rescaledX_test = scaler.transform(X_test)
 
-# Create a Gaussian Classifier
-model = GaussianNB()
-model.fit(r.values, target)
+rescaledX_Data = scaler.fit_transform(r.values)
 
-# # # Predict the response for test dataset
-y_pred = model.predict(X_test)
-# #
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 
-# Model Accuracy, how often is the classifier correct?
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+rf = RandomForestClassifier(n_estimators=500)
+rf.fit(rescaledX_Data, target)
+y_pred = rf.predict(rescaledX_test)
+print("Random Forest classifier has accuracy of: ", rf.score(rescaledX_test, y_test))
+
+# Evaluate the confusion_matrix
+# confusion_matrix(y_test, y_pred)
 
 # read training dataset
 # test_data = pd.read_excel('testdata.xlsx', header=None)
 test_data = pd.read_csv('testdata.csv', delimiter=',')
 # test_data.columns = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15']
-print(test_data)
+# print(test_data)
 
 original_data = test_data.copy()
 
 b = pre_processing(test_data, type='test')
-test_pred = model.predict(b.values)
+
+rescaledX_testData = scaler.transform(b.values)
+
+# test_pred = rf.predict(b.values)
+
+# print()
+# print("The predicted results for the test data")
+# print(le.inverse_transform(test_pred))
+
+# original_data['A16'] = le.inverse_transform(test_pred)
+# original_data.to_csv("predictions2.csv",index=False)
+
+# # print("Random Forest classifier has accuracy of: ", rf.score(rescaledX_test, y_test))
+
+# res = pd.DataFrame({ 'id' : range(1, test_pred.size+1 ,1)})
+# res['Category'] = le.inverse_transform(test_pred)
+# res.to_csv("res.csv",index=False)
+
+from sklearn.linear_model import LogisticRegression
+
+# Fitting logistic regression with default parameter values
+logreg = LogisticRegression()
+logreg.fit(rescaledX_Data, target)
+
+# Using the trained model to predict instances from the test set
+y_pred = logreg.predict(rescaledX_test)
+
+# Getting the accuracy score of predictive model
+print("Logistic regression classifier has accuracy of: ", logreg.score(rescaledX_test, y_test))
+
+test_pred = logreg.predict(rescaledX_testData)
 
 print()
 print("The predicted results for the test data")
 print(le.inverse_transform(test_pred))
 
 original_data['A16'] = le.inverse_transform(test_pred)
-original_data.to_csv("Group11_Predictions.csv",index=False)
+# original_data.to_csv("predictions2.csv",index=False)
 
 res = pd.DataFrame({ 'id' : range(1, test_pred.size+1 ,1)})
 res['Category'] = le.inverse_transform(test_pred)
-res.to_csv("res.csv",index=False)
+res.to_csv("res2.csv",index=False)
+
